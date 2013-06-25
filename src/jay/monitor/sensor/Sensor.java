1cfd9c0d4ef580487988276206fad134c5737da5
Joachim Schlienger <joachim.schlienger@baloise.com>
2013-06-25 10:12:12 +0200

    cleanup gh-pages

--- src/jay/monitor/sensor/Sensor.java
+++
@@ -1,9 +0,0 @@
-package jay.monitor.sensor;
-
-import java.beans.PropertyChangeListener;
-
-public interface Sensor extends Runnable{
-	public void addPropertyChangeListener(PropertyChangeListener listener);
-	public String getName();
-	public double getValue();
-}
