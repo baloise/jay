994c11e624d7c83dd0450cdcf8982ea0827b8020
Matthias Cullmann <matthias.cullmann@gmail.com>
2015-03-22 17:32:17 +0100

    simplified dummy sensor

--- src/jay/monitor/sensor/DummySensor.java
+++ src/jay/monitor/sensor/DummySensor.java
@@ -1,42 +1,23 @@
 package jay.monitor.sensor;
 
-import java.beans.PropertyChangeEvent;
 import java.util.Properties;
 import java.util.Random;
 
 public class DummySensor extends AbstractSensor {
 	Random random = new Random(System.currentTimeMillis());
-	int delay=10;
-	float factor=1;
-	double value;
+
 	public DummySensor(Properties props) {
-	    super(props);
+		super(props);
 	}
-	
+
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	public void run() {
-		while(true){
-			try {
-				Thread.sleep((long) (random.nextInt(delay*1000)*factor));
-			} catch (InterruptedException e) {
-			}
-			delay+=10;
-			factor *=1.2;
-      value = (value + 1) % 2;
-      firePropertyChangeEvent(new PropertyChangeEvent(this, "sensor", (value + 1) % 2, value));
+	protected double sens() {
+		if (getDelay() < 30000) {
+			setDelay(random.nextInt(2000) + getDelay());
 		}
+		return (getValue() + 1) % 2;
 	}
-
-	@Override
-	public double getValue() {
-    return value;
-	}
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  protected double sens() {
-    return 0;
-  }
 }
