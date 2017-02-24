348e86872bd8ea5661faa464d9929529a9b67833
Matthias Cullmann <matthias.cullmann@baloise.ch>
2017-02-24 11:21:01 +0100

    CLEAN

--- jay-api/src/jay/monitor/sensor/Sensor.java
+++ jay-api/src/jay/monitor/sensor/Sensor.java
@@ -3,7 +3,7 @@ package jay.monitor.sensor;
 import java.beans.PropertyChangeListener;
 
 public interface Sensor extends Runnable{
-	public void addPropertyChangeListener(PropertyChangeListener listener);
-	public String getName();
-	public double getValue();
+	void addPropertyChangeListener(PropertyChangeListener listener);
+	String getName();
+	double getValue();
 }
